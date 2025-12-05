93e4a021b845d33a9477fb414b497ade3007e4fa
Aman Sharma <amandanoda7@gmail.com>
2025-12-05 19:04:08 +0530

    Add files via upload

---
+++ TicToeAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  TicToeAppApp.swift
+//  TicToeApp
+//
+//  Created by Ravi Sharma on 02/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+
+struct TicToeAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TickityTacTowApp.swift
@@ -0,0 +1,19 @@
+        
+//  TickityTacTowApp.swift
+//  TicToeApp
+//
+//  Created by Ravi Sharma on 02/10/25.
+//
+
+
+
+import SwiftUI
+
+@main
+struct TickityTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
